37bd67720f85ead0461b7f25b99a8a87f115911f
Angely <chavezangely1@gmail.com>
2022-03-06 23:07:57 -0300

    estoy subiendo index y script

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hola Js")
+
+var cantidadDeVentanasEnMiCasa = 3
+var soyHumano = true
+var miGustoDePizzaPreferido = ("pepperoni")
+var mismaVariable = "Hola";
+
+
+console.log (cantidadDeVentanasEnMiCasa)
+console.log (soyHumano)
+console.log ("Mi gusto de pizza preferido es" + " " + miGustoDePizzaPreferido)
+console.log (mismaVariable)
+
+var mismaVariable = 0
+console.log(mismaVariable);
+
+var mismaVariable = "Minions"
+console.log(mismaVariable);
